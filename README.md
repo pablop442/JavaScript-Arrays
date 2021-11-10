cb13af8db1c961d7eaddd4d7d11bdb1e56bee658
pablop442 <pablo442@gmail.com>
2021-11-10 07:40:45 +0000

    added folders and answers

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ejercicios programación funcional JS (clase 7: 03/11/2021)
+
+[![JS](https://www.hexacta.com/wp-content/uploads/2015/11/Functional-programming.jpg "JS")](https://www.hexacta.com/wp-content/uploads/2015/11/Functional-programming.jpg "JS")
+
+### Descripción
+
+1. Este proyecto consta de cuatro ejercicios de JavaScript.
+2. Cada ejercicio fue solucionado atendiendo a principios de programación funcional, haciendo uso de programación imperativa y declarativa según cada caso.
+3. Estos ejercicios fueron realizados con fines académicos para el bootcamp de 4Geeks Academy.
+
+### Ejecución del proyecto
+
+1. Asegurate de estar situado en el directorio del proyecto (scripts).
+2. Ejecuta `node index.js` en la terminal.
+
+
+### Enunciado de los Ejercicios
+
+1. Crea una funcion que encuentre el valor 4 en el array exampleArray 
+ - Debes crear un algoritmo usando programación funcional 
+ - Debes crear otro algoritmo usando programación declarativa (filter, split, reduce o cualquierda de ellas que manipulen arrays)
+2. Crea una funcion que limpie el array shit usando programación declarativa (split, join o cualquiera de ellas). La función solo debe dejar los strings como resultado.
+3. Crea una función que calcule la distancia entre dos puntos usando el objeto que yo te proporcionono como Point, debes usar programación imperativa para este ejericio. Tu funcion debe recibir dos parametros, punto a y punto b y calcular la distancia.
+4. Crear una función que compare los valores de exampleArray2 y exampleArray1 pero que se fije en el tipo (y2-y1, x2-x1)
+
---
+++ assets/index.js
@@ -0,0 +1,91 @@
+// example data
+const exampleArray = [10, 4, 100, 35, 31, 23, 443, 221, 342, 10, 12, 42];
+const shit = ["function", null, function() {}, () => {}, 10, 100, {}];
+const exampleArray2 = [
+    [10, 4, "100", 35, "31", "23", 443, "221", "342", 10, 12, 42]
+]
+
+//object
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+//-------------------------------------------------------Soluciones-----------------------------------------------------
+
+// Esta función limpia cualuqier array y deja solo números aunque sea un array dentro de otro (como es el caso de exampleArray2)
+const arrayCleaner=(array)=>{
+ return oneArr(array).filter(value=> typeof value === 'number'); 
+};
+
+
+//Ejercicio 1: Crea una funcion que encuentre el valor 4 en el array exampleArray.
+
+//Programación funcional
+// Función recorre todo el array en busca del número 4. Cuando lo consigue retorna el index en el que se encuentra.
+const numFourFinder = (arr) =>{
+    for(let i = 0; i<arr.length; i++){
+        if (arr[i] === 4){
+            return i;
+        };
+    };
+};
+console.log (numFourFinder(exampleArray))
+
+//Programación declarativa
+//Usando la función map pido que imprima el indice de cualquier valor que sea 4.
+exampleArray.map((value,index)=>{
+    if(value === 4){
+        console.log (index);
+    };
+});
+
+//Ejercicio 2: Crea una funcion que limpie el array shit usando programación declarativa
+
+//Función filtra los elementos del array de tipo string y luego los retorna dentro del array.
+const theStringFilter = (arr) =>{  
+    let stringOnly= arr.filter(value=> typeof value === 'string');
+    return stringOnly;
+};
+
+ console.log(theStringFilter(shit));
+
+ //Ejercicio 3: Crea una función que calcule la distancia entre dos puntos. 
+
+ // Declaro variables que definen X y Y. Función que reciba dos puntos.
+ let a = new Point(5, 5);
+ let b = new Point(9, 6); 
+
+//Creo una función que mida la distancia entre dos puntos. Retorno un new point para que retorne un nuevo objeto.
+ const distanceMeasure = (pointA, pointB) =>{
+     return new Point((pointB.x - pointA.x),(pointB.y - pointA.y)); 
+ };
+console.log (distanceMeasure(a, b));
+
+ //Ejercicio 4: Crear una función que compare los valores de exampleArray2 y exampleArray1 pero que se fije en el tipo. Si el array es más corto debe parar
+ // de comparar en el de menor longitud. 
+ 
+ //Creo una función que verifique que los arrays tengan la misma longitud y que mientras esa condición sea cierta, compare sus index y se detenga en lo que sean diferentes.
+const arrayCompare=(arr1, arr2)=>{
+    let cleanArr2 = oneArr(arr2);
+    let stopIndex = 0;
+    if(arr1.length === cleanArr2.length){
+        let i = 0;
+        while(arr1[i] === cleanArr2[i]){
+            i++;
+            stopIndex = i;    
+        }
+    }  
+    return 'Los arrays son diferentes en el indice '+ stopIndex
+}
+
+//Función que recorre arrays anidados y deja un solo array.
+const oneArr =(arr) =>{
+    for(i in arr){
+        for(j in arr[i]){
+            let indexJ = arr[j];
+            return indexJ;
+        };
+    };  
+};
+
+console.log(arrayCompare(exampleArray,exampleArray2));
